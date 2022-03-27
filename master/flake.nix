5ef862455fa1cb53974d419aec095b9300710865
hugosenari <hugosenari@gmail.com>
2022-03-27 17:58:59 -0300

    chore: re index bpg

--- master/flake.nix
+++ master/flake.nix
@@ -11,6 +11,7 @@
   inputs.src-bpg-master.ref   = "refs/heads/master";
   inputs.src-bpg-master.owner = "def-";
   inputs.src-bpg-master.repo  = "nim-bpg";
+  inputs.src-bpg-master.dir   = "";
   inputs.src-bpg-master.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
